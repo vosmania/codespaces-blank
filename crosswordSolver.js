61d7b2c57a55b1760c63c0169dc12ef5f9eea28d
vosmania <81016932+vosmania@users.noreply.github.com>
2024-03-04 00:58:38 +0000

    Removed the console.log() from the crosswordSolver.js file ```

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -122,33 +122,3 @@ function crosswordSolver(puzzleString, words) {
         return true;
     }
 }
-
-const puzzle = `...1...........
-..1000001000...
-...0....0......
-.1......0...1..
-.0....100000000
-100000..0...0..
-.0.....1001000.
-.0.1....0.0....
-.10000000.0....
-.0.0......0....
-.0.0.....100...
-...0......0....
-..........0....`
-const words = [
-  'sun',
-  'sunglasses',
-  'suncream',
-  'swimming',
-  'bikini',
-  'beach',
-  'icecream',
-  'tan',
-  'deckchair',
-  'sand',
-  'seaside',
-  'sandals',
-].reverse()
-
-console.log(crosswordSolver(puzzle, words));
